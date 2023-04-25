40d12a6d7b198c48a1328cae6c287f89deccb7dc
CSB <tph5268@naver.com>
2023-04-25 18:01:44 +0900

    scope

--- study/core/src/test/java/hello/core/lifecycle/NetworkClient.java
+++ study/core/src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,9 +1,10 @@
 package hello.core.lifecycle;
 
-import org.springframework.beans.factory.DisposableBean;
-import org.springframework.beans.factory.InitializingBean;
 
-public class NetworkClient implements InitializingBean, DisposableBean {
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+public class NetworkClient {
 
     private String url;
 
@@ -29,16 +30,16 @@ public class NetworkClient implements InitializingBean, DisposableBean {
         System.out.println("close: " + url);
     }
 
-    @Override
-    public void afterPropertiesSet() throws Exception {
-        System.out.println("Network.connect");
+    @PostConstruct
+    public void init() {
+        System.out.println("Network.init");
         connect();
         call("초기화 연결 메시지");
     }
 
-    @Override
-    public void destroy() throws Exception {
-        System.out.println("Network.destroy");
+    @PreDestroy
+    public void close() {
+        System.out.println("Network.close");
         disconnect();
     }
 }
