0064466ab0746f436db7237290d0e1282d6f86a4
kd <csb5268@gmail.com>
2023-08-24 02:22:32 +0900

    AOP

--- study/jdbc/src/main/java/hello/jdbc/service/MemberServiceV3_3.java
+++ study/jdbc/src/main/java/hello/jdbc/service/MemberServiceV3_3.java
@@ -5,8 +5,7 @@ import hello.jdbc.repository.MemberRepositoryV3;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.transaction.PlatformTransactionManager;
-import org.springframework.transaction.TransactionStatus;
-import org.springframework.transaction.support.DefaultTransactionDefinition;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.transaction.support.TransactionTemplate;
 
 import java.sql.Connection;
@@ -14,25 +13,12 @@ import java.sql.SQLException;
 
 @Slf4j
 @RequiredArgsConstructor
-public class MemberServiceV3_2 {
-
-    private final TransactionTemplate txTamplate;
+public class MemberServiceV3_3 {
     private final MemberRepositoryV3 memberRepository;
 
-    public MemberServiceV3_2(PlatformTransactionManager transactionManager, MemberRepositoryV3 memberRepository) {
-        this.txTamplate = new TransactionTemplate(transactionManager);
-        this.memberRepository = memberRepository;
-    }
-
+    @Transactional
     public void accountTransfer(String fromId, String toId, int money) throws SQLException {
-        txTamplate.executeWithoutResult((status) -> {
-            // 비즈니스 로직
-            try {
-                bizLogic(fromId, toId, money);
-            } catch (SQLException e) {
-                throw new IllegalStateException(e);
-            }
-        });
+        bizLogic(fromId, toId, money);
     }
 
     private void bizLogic(String fromId, String toId, int money) throws SQLException {
